76ffcf7a2d6d70ef664bee0e6be3ad723b3213e8
Bojan Gabric <bojan@bojangabric.com>
2020-09-29 11:04:11 +0200

    Add api for fetching posts

---
+++ pages/api/posts/index.js
@@ -0,0 +1,13 @@
+import nextConnect from 'next-connect';
+import middleware from '../../../lib/database';
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  let doc = await req.db.collection('posts').find({}).toArray();
+  res.json(doc);
+});
+
+export default handler;
