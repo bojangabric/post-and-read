e8ee45865136003fb99154264aa60c8391e7d672
Bojan Gabric <bojan@bojangabric.com>
2020-11-26 21:48:03 +0100

    Add avatar and date to create api

--- pages/api/posts/create.js
+++ pages/api/posts/create.js
@@ -1,15 +1,21 @@
 import nextConnect from 'next-connect';
 import middleware from '../../../lib/database';
+import { format } from 'date-fns';
 
 const handler = nextConnect();
 
 handler.use(middleware);
 
 handler.post(async (req, res) => {
-  const { title, post, author } = req.body;
-  let doc = await req.db
-    .collection('posts')
-    .insertOne({ title: title, post: post, author: author, picture: 'https://via.placeholder.com/1920x1080' });
+  const { title, post, author, picture, avatar } = req.body;
+  let doc = await req.db.collection('posts').insertOne({
+    title: title,
+    post: post,
+    author: author,
+    picture: picture,
+    avatar: avatar,
+    date: format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")
+  });
   res.json(doc);
 });
 
