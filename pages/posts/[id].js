8d7d0201fad2c924a0e652ab29836bae1898616c
Bojan Gabric <bojan@bojangabric.com>
2020-11-28 01:55:55 +0100

    Add SEO to post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
+import { NextSeo } from 'next-seo';
 
 const Post = ({ post }) => {
   return (
-    <div className="markdown mt-32">
-      <ReactMarkdown source={post.post} />
-    </div>
+    <>
+      <NextSeo title={post.title} />
+      <div className="mt-32 max-w-2xl mx-auto">
+        <img src={post.picture} />
+        <div className="mt-16 text-4xl font-bold">{post.title}</div>
+        <div className="mt-4 mb-24">Written by {post.author}</div>
+        <div className="markdown">
+          <ReactMarkdown source={post.post} />
+        </div>
+      </div>
+    </>
   );
 };
 
